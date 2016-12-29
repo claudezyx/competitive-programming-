9f1894421f84ad8afd554e6810dd4a897f84422b
Ping Wu <claude.zhao@mail.utoronto.ca>
2016-12-28 22:13:53 -0500

    quickSort

---
+++ quickSort.py
@@ -0,0 +1,28 @@
+class QuickSort():
+  def partition(self, array, start, end): 
+    if (start > end): return None 
+    elif (start == end): return start 
+    else:
+      pivot = array[end]
+      p = start-1 
+      for i in range(start, end):
+        if (array[i] <= pivot): 
+          p += 1 
+          array[p], array[i] = array[i], array[p]
+          
+      p += 1 
+      array[p], array[end] = array[end], array[p]
+      return p 
+    
+  def QSort(self,array,start,end): 
+    
+    if (start >= end): return array 
+    pivot = self.partition(array, start, end)
+    self.partition(array, start, pivot-1)
+    self.partition(array, pivot+1, end)
+    
+    return array 
+
+a = QuickSort()
+b = a.QSort([3,5,10,4,8,11,7],2,3)
+print(b)
