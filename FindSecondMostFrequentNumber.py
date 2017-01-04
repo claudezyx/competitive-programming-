6db3c9d387968f973def3169fdd27bbbb8a935c5
claudezyx <claude.zhao@mail.utoronto.ca>
2017-01-03 22:45:48 -0500

    Update and rename Find the second most frequent number to FindSecondMostFrequentNumber.py

--- FindSecondMostFrequentNumber.py
+++ FindSecondMostFrequentNumber.py
@@ -1,6 +1,8 @@
 """
 Given a list of integers, return the number that appears second most frequently.
 For example, [0,1,2,2,2,2,1,0,5,1] would return 1 
+Python Collection Module 
+https://pymotw.com/2/collections/counter.html
 """
 import collections 
 class Solution(): 
